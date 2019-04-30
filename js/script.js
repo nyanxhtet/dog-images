e276001344b36fd633bca3cd6a85f285152605fd
nyanxhtet <nyanhtet@yahoo.com>
2019-04-30 15:59:19 -0500

    reindendted script.js

--- js/script.js
+++ js/script.js
@@ -1,85 +1,85 @@
-	var dogs = (function(){
+var dogs = (function(){
 
-	  var $breeds = $('#breeds');
-	  var $imageContainer= $('#imageContainer');
-	  var $closeButton = $('#closeButton');
-
-
-	  function getBreedList (){
-
-	    $.ajax({
-	      type:'GET',
-	      url: 'https://dog.ceo/api/breeds/list/all',
-	      dataType:'text',
-	      success: function(breed){
-	        var breedsObj = JSON.parse(breed);
-	        $.each(breedsObj.message, function(key, value){
-	          $breeds.append('<option value='+key+'\'>'+key+'</option>;');
-	        });
-	        $('#submitButton').on('click',function(){
-	          dogs.getBreedImage();
-	        });
-	      }
-	    });
-	  };
-
-	 function getBreedImage(){
-	   $.ajax({
-	     type:'GET',
-	     url:'https://dog.ceo/api/breed/'+ ($("#breeds option:selected").text()) +'/images/random',
-	     dataType:'text',
-	     success: function(response){
-
-	       showModal();
-
-	       var imageUrl = JSON.parse(response).message;
-
-	       $imageContainer.append('<div class=\'modal\'> </div>');
-
-	       $('.modal').append('<img class=\'image\' src='+imageUrl+'>');
-
-	       $('.modal').append('<button class=\'modal-close\'> Close </button>');
-
-	       $('.modal-close').on('click', function(){
-	         hideModal();
-	       });
-	     }});
-
-	   };
-
-	    function showModal(){
-	      $imageContainer.addClass('visible');
-	    }
-
-	    function hideModal(){
-	      $imageContainer.removeClass('visible');
-	      $imageContainer.html('');
-	    }
-	    $imageContainer.on('click', function (e) {
-	      var $target = e.target;
-	      if ($target === $imageContainer[0]){
-	        hideModal();
-	      }
-	    });
+  var $breeds = $('#breeds');
+  var $imageContainer= $('#imageContainer');
+  var $closeButton = $('#closeButton');
 
 
-	    return{
-	      getBreedList: getBreedList,
-	      getBreedImage: getBreedImage,
-	      showModal: showModal,
-	      hideModal: hideModal,
-	    };
+  function getBreedList (){
 
-	  })();
+    $.ajax({
+      type:'GET',
+      url: 'https://dog.ceo/api/breeds/list/all',
+      dataType:'text',
+      success: function(breed){
+        var breedsObj = JSON.parse(breed);
+        $.each(breedsObj.message, function(key, value){
+          $breeds.append('<option value='+key+'\'>'+key+'</option>;');
+        });
+        $('#submitButton').on('click',function(){
+          dogs.getBreedImage();
+        });
+      }
+    });
+  };
+
+  function getBreedImage(){
+    $.ajax({
+      type:'GET',
+      url:'https://dog.ceo/api/breed/'+ ($("#breeds option:selected").text()) +'/images/random',
+      dataType:'text',
+      success: function(response){
+
+        showModal();
+
+        var imageUrl = JSON.parse(response).message;
+
+        $imageContainer.append('<div class=\'modal\'> </div>');
+
+        $('.modal').append('<img class=\'image\' src='+imageUrl+'>');
+
+        $('.modal').append('<button class=\'modal-close\'> Close </button>');
+
+        $('.modal-close').on('click', function(){
+          hideModal();
+        });
+      }});
+
+    };
+
+    function showModal(){
+      $imageContainer.addClass('visible');
+    }
+
+    function hideModal(){
+      $imageContainer.removeClass('visible');
+      $imageContainer.html('');
+    }
+    $imageContainer.on('click', function (e) {
+      var $target = e.target;
+      if ($target === $imageContainer[0]){
+        hideModal();
+      }
+    });
+
+
+    return{
+      getBreedList: getBreedList,
+      getBreedImage: getBreedImage,
+      showModal: showModal,
+      hideModal: hideModal,
+    };
+
+  })();
 
 
 
-	  $(document).ready(function(){
-	    dogs.getBreedList();
-	    $(document).on('keyup', function(e){
-	      if(e.keyCode === 27){
-	        dogs.hideModal();
-	      }
-	    })
+  $(document).ready(function(){
+    dogs.getBreedList();
+    $(document).on('keyup', function(e){
+      if(e.keyCode === 27){
+        dogs.hideModal();
+      }
+    })
 
-	  });
+  });
