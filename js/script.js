9f019ab800b1a9411ce956eaa6542e5d4872011e
nyanxhtet <nyanhtet@yahoo.com>
2019-04-30 08:37:36 -0500

    reindented index.html & script.js

--- js/script.js
+++ js/script.js
@@ -8,78 +8,78 @@ var dogs = (function(){
   function getBreedList (){
 
     $.ajax({
-     type:'GET',
-     url: 'https://dog.ceo/api/breeds/list/all',
-     dataType:'text',
-     success: function(breed){
-       var breedsObj = JSON.parse(breed);
-       $.each(breedsObj.message, function(key, value){
-         $breeds.append('<option value='+key+'\'>'+key+'</option>;');
-       });
-       $('#submitButton').on('click',function(){
-         dogs.getBreedImage();
-       });
-       }
-     });
-   };
+      type:'GET',
+      url: 'https://dog.ceo/api/breeds/list/all',
+      dataType:'text',
+      success: function(breed){
+        var breedsObj = JSON.parse(breed);
+        $.each(breedsObj.message, function(key, value){
+          $breeds.append('<option value='+key+'\'>'+key+'</option>;');
+        });
+        $('#submitButton').on('click',function(){
+          dogs.getBreedImage();
+        });
+      }
+    });
+  };
 
-   function getBreedImage(){
-       $.ajax({
-         type:'GET',
-         url:'https://dog.ceo/api/breed/'+ ($("#breeds option:selected").text()) +'/images/random',
-         dataType:'text',
-         success: function(response){
+  function getBreedImage(){
+    $.ajax({
+      type:'GET',
+      url:'https://dog.ceo/api/breed/'+ ($("#breeds option:selected").text()) +'/images/random',
+      dataType:'text',
+      success: function(response){
 
-           showModal();
+        showModal();
 
-           var imageUrl = JSON.parse(response).message;
+        var imageUrl = JSON.parse(response).message;
 
-           $imageContainer.append('<div class=\'modal\'> </div>');
+        $imageContainer.append('<div class=\'modal\'> </div>');
 
-           $('.modal').append('<img class=\'image\' src='+imageUrl+'>');
+        $('.modal').append('<img class=\'image\' src='+imageUrl+'>');
 
-           $('.modal').append('<button class=\'modal-close\'> Close </button>');
+        $('.modal').append('<button class=\'modal-close\'> Close </button>');
 
-           $('.modal-close').on('click', function(){
-             hideModal();
-           });
-         }});
+        $('.modal-close').on('click', function(){
+          hideModal();
+        });
+      }});
 
-      };
+    };
 
-  function showModal(){
-    $imageContainer.addClass('visible');
-  }
+    function showModal(){
+      $imageContainer.addClass('visible');
+    }
 
-  function hideModal(){
-    $imageContainer.removeClass('visible');
-    $imageContainer.html('');
-  }
-  $imageContainer.on('click', function (e) {
+    function hideModal(){
+      $imageContainer.removeClass('visible');
+      $imageContainer.html('');
+    }
+    $imageContainer.on('click', function (e) {
       var $target = e.target;
       if ($target === $imageContainer[0]){
         hideModal();
       }
+    });
+
+
+    return{
+      getBreedList: getBreedList,
+      getBreedImage: getBreedImage,
+      showModal: showModal,
+      hideModal: hideModal,
+    };
+
+  })();
+
+  dogs.getBreedList();
+
+  $(document).ready(function(){
+
+    $(document).on('keyup', function(e){
+      if(e.keyCode === 27){
+        dogs.hideModal();
+      }
+    })
+
   });
-
-
-return{
-  getBreedList: getBreedList,
-  getBreedImage: getBreedImage,
-  showModal: showModal,
-  hideModal: hideModal,
-};
-
-})();
-
-dogs.getBreedList();
-
-$(document).ready(function(){
-
-$(document).on('keyup', function(e){
-  if(e.keyCode === 27){
-    dogs.hideModal();
-  }
-})
-
-});
