42459bf1d9aec3db13960ed9189f64043779ced2
nyanxhtet <nyanhtet@yahoo.com>
2019-05-21 11:02:37 -0500

    Used prettier to format the Javascript document.

--- js/script.js
+++ js/script.js
@@ -1,82 +1,78 @@
-var dogs = (function(){
+var dogs = (function() {
+  var $breeds = $("#breeds");
+  var $imageContainer = $("#imageContainer");
 
-  var $breeds = $('#breeds');
-  var $imageContainer= $('#imageContainer');
-
-  function getBreedList (){
+  function getBreedList() {
     $.ajax({
-      type:'GET',
-      url: 'https://dog.ceo/api/breeds/list/all',
-      dataType:'text',
-      success: function(breed){
+      type: "GET",
+      url: "https://dog.ceo/api/breeds/list/all",
+      dataType: "text",
+      success: function(breed) {
         var breedsObj = JSON.parse(breed);
-        $.each(breedsObj.message, function(key){
-          $breeds.append('<option value='+key+'\'>'+key+'</option>;');
+        $.each(breedsObj.message, function(key) {
+          $breeds.append("<option value=" + key + "'>" + key + "</option>;");
         });
-        $('#submitButton').on('click',function(){
+        $("#submitButton").on("click", function() {
           dogs.getBreedImage();
         });
       }
     });
   }
 
-  function getBreedImage(){
+  function getBreedImage() {
     $.ajax({
-      type:'GET',
-      url:'https://dog.ceo/api/breed/'+ ($('#breeds option:selected').text()) +'/images/random',
-      dataType:'text',
-      success: function(response){
-
+      type: "GET",
+      url:
+        "https://dog.ceo/api/breed/" +
+        $("#breeds option:selected").text() +
+        "/images/random",
+      dataType: "text",
+      success: function(response) {
         showModal();
 
         var imageUrl = JSON.parse(response).message;
 
-        $imageContainer.append('<div class=\'modal\'> </div>');
+        $imageContainer.append("<div class='modal'> </div>");
 
-        $('.modal').append('<img class=\'image\' src='+imageUrl+'>');
+        $(".modal").append("<img class='image' src=" + imageUrl + ">");
 
-        $('.modal').append('<button class=\'modal-close\'> Close </button>');
+        $(".modal").append("<button class='modal-close'> Close </button>");
 
-        $('.modal-close').on('click', function(){
+        $(".modal-close").on("click", function() {
           hideModal();
         });
-      }});
-
-    }
-
-    function showModal(){
-      $imageContainer.addClass('visible');
-    }
-
-    function hideModal(){
-      $imageContainer.removeClass('visible');
-      $imageContainer.html('');
-    }
-    $imageContainer.on('click', function (e) {
-      var $target = e.target;
-      if ($target === $imageContainer[0]){
-        hideModal();
       }
     });
+  }
 
+  function showModal() {
+    $imageContainer.addClass("visible");
+  }
 
-    return{
-      getBreedList: getBreedList,
-      getBreedImage: getBreedImage,
-      showModal: showModal,
-      hideModal: hideModal,
-    };
-
-  })();
-
-
-
-  $(document).ready(function(){
-    dogs.getBreedList();
-    $(document).on('keyup', function(e){
-      if(e.keyCode === 27){
-        dogs.hideModal();
-      }
-    })
-
+  function hideModal() {
+    $imageContainer.removeClass("visible");
+    $imageContainer.html("");
+  }
+  $imageContainer.on("click", function(e) {
+    var $target = e.target;
+    if ($target === $imageContainer[0]) {
+      hideModal();
+    }
   });
+
+  return {
+    getBreedList: getBreedList,
+    getBreedImage: getBreedImage,
+    showModal: showModal,
+    hideModal: hideModal
+  };
+})();
+
+$(document).ready(function() {
+  dogs.getBreedList();
+  $(document).on("keyup", function(e) {
+    if (e.keyCode === 27) {
+      dogs.hideModal();
+    }
+  });
+});
